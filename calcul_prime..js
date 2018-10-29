1701ac5c10ca6ef70dfe1f04b21ac1c362794120
newparts <iuliu@newparts.ro>
2018-10-29 14:26:51 +0200

    stop edit

--- calcul_prime..js
+++ calcul_prime..js
@@ -1,3 +1,4 @@
+var stopped = false;
 onmessage = function(e){
     if (e.data=="stop"){
 
@@ -11,8 +12,17 @@ function is_prim(n)
 	return true;
 }
 //-------------------------------
-for (var i = 1e9; i < 1e10; i++)
+function calculeaza_prime(strat_value)
+{
+    if(stopped);
+    else{
+for (var i = strat_value; i < strat_value + 1000; i++)
 	if (is_prim(i))
-		postMessage(i);
-
+        postMessage(i);
+        strat_value +=1000;
+    }
+    if(strat_value<1e10)
+    setInterval(calculeaza_prime, 1, strat_value);
+else
 postMessage("gata");
+}
