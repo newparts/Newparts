1a0aca022a2cc05fd467a3765a093fec3a4689e2
newparts <iuliu@newparts.ro>
2018-10-29 15:10:40 +0200

    start

--- calcul_prime..js
+++ calcul_prime..js
@@ -1,28 +1,39 @@
 var stopped = false;
-onmessage = function(e){
-    if (e.data=="stop"){
 
-    }
+//------------------------------ 
+onmessage = function(e){
+	if (e.data == "stop"){
+		stopped = true;
+	}
+	else
+		if (e.data == "start"){
+			stopped = false;
+		}
 }
+//------------------------------ 
 function is_prim(n)
 {
 	for (var i = 2; i * i <= n; i++)
 		if (n % i == 0)
 			return false;
+		
 	return true;
 }
-//-------------------------------
-function calculeaza_prime(strat_value)
+//------------------------------
+function calculeaza_prime(start_value)
 {
-    if(stopped);
-    else{
-for (var i = strat_value; i < strat_value + 1000; i++)
-	if (is_prim(i))
-        postMessage(i);
-        strat_value +=1000;
-    }
-    if(strat_value<1e10)
-    setInterval(calculeaza_prime, 1, strat_value);
-else
-postMessage("gata");
+	if (stopped)
+		;
+	else{
+		for (var i = start_value; i < start_value + 1000; i++)
+			if (is_prim(i))
+				postMessage(i);
+		start_value += 1000;	
+	}
+	if (start_value < 1e10)
+		setTimeout(calculeaza_prime, 1, start_value);
+	else
+		postMessage("gata");
 }
+//------------------------------
+calculeaza_prime(1e9);
