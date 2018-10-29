4c8d1685c1acd894e2fa882facc41505ce5c4d9c
newparts <iuliu@newparts.ro>
2018-10-29 15:16:52 +0200

    renew

--- calcul_prime..js
+++ calcul_prime..js
@@ -1,39 +1,39 @@
-var stopped = false;
+var stopped=false;
 
-//------------------------------ 
-onmessage = function(e){
-	if (e.data == "stop"){
-		stopped = true;
-	}
-	else
-		if (e.data == "start"){
-			stopped = false;
-		}
-}
-//------------------------------ 
-function is_prim(n)
+onmessage = function(e)
 {
-	for (var i = 2; i * i <= n; i++)
-		if (n % i == 0)
+	if(e.data == "stop")
+	{
+        stopped=true;
+	}
+	else if(e.data == "start")
+	{
+        stopped=false;
+	}
+}
+//----------------------
+function is_prime(n)
+{
+	for(var i=2;i*i<=n;i++)
+		if(n%i==0)
 			return false;
-		
 	return true;
 }
-//------------------------------
 function calculeaza_prime(start_value)
 {
-	if (stopped)
+	if(stopped)
 		;
-	else{
-		for (var i = start_value; i < start_value + 1000; i++)
-			if (is_prim(i))
+	else
+	{
+		for (var i=start_value; i<start_value+1000; i++)
+			if(is_prime(i))
 				postMessage(i);
-		start_value += 1000;	
+			start_value+=1000;
 	}
-	if (start_value < 1e10)
+	if(start_value<1e10)
 		setTimeout(calculeaza_prime, 1, start_value);
 	else
 		postMessage("gata");
 }
-//------------------------------
+//////////////////////////
 calculeaza_prime(1e9);
--- desenare.js
+++ desenare.js
@@ -1,53 +1,51 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.29.4";
+document.getElementById("id_logic_version").innerHTML = "Bussiness version: 2018.10.29.2";
 document.getElementById("id_start_button").addEventListener("click", start);
 document.getElementById("id_stop_button").addEventListener("click", stop);
-
 document.getElementById("id_start_button").disabled = false;
 document.getElementById("id_stop_button").disabled = true;
-
-var unghi = {valoare:0};
-var my_worker = null;
-//-----------------------------------
-function deseneaza_cerc(context, w, h, unghi)
+var unghi={valoare:0};
+var my_worker=null;
+//-----------------------------------------
+function deseneaza_cerc(context, width, height, unghi)
 {
-	context.clearRect(0, 0, w, h);
-	
+	context.clearRect(0,0,width,height);
 	context.beginPath();
-	context.arc(w / 2 + 100 * Math.cos(unghi.valoare * Math.PI / 180), 
-				h / 2 + Math.sin(unghi.valoare * Math.PI / 180) * 100, 
-				50, 0, 2 * Math.PI);
-	
-	context.stroke();
-	
+	context.arc(width/2+100*Math.cos(unghi.valoare*Math.PI / 180), height/2 + Math.sin(unghi.valoare*Math.PI / 180)*100, 50, 0, 2*Math.PI); //adauga la path
+
+	context.stroke(); // desenam
 	unghi.valoare++;
 }
-//-----------------------------------
+//-----------------------------------------
 function start()
 {
 	var canvas = document.getElementById("id_canvas");
 	var context = canvas.getContext("2d");
 	
+	
 	document.getElementById("id_start_button").disabled = true;
 	document.getElementById("id_stop_button").disabled = false;
-	
-	if (my_worker == null){
+	if(my_worker== null)
+	{
 		my_worker = new Worker("calcul_prime.js");
-		my_worker.onmessage = function(e){
+		my_worker.onmessage = function(e)
+		{
 			document.getElementById("id_prime").innerHTML = e.data;
+
 		}
 	}
 	else
 		my_worker.postMessage("start");
+	id_timer = setInterval(deseneaza_cerc, 10 , context,canvas.width , canvas.height, unghi )
 	
-	id_timer = setInterval(deseneaza_cerc, 10, context, canvas.width, canvas.height, unghi);
+	
 }
-//-----------------------------------
+//------------------------------------[]
 function stop()
 {
 	document.getElementById("id_start_button").disabled = false;
-	document.getElementById("id_stop_button").disabled = true;	
-	
+	document.getElementById("id_stop_button").disabled = true;
 	clearInterval(id_timer);
+
 	my_worker.postMessage("stop");
+
 }
-//-----------------------------------
